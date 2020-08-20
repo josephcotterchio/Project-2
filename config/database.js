885452140627fe1d0d569b63181414644fb97cb8
Joe Cotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-08-20 18:18:01 -0400

    "still trying"

--- config/database.js
+++ config/database.js
@@ -12,14 +12,3 @@ db.on("connected", function () {
 db.on("err", function () {
   console.log("Mongo has thrown an error", err);
 });
-
-
-const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://joecotterchio:Gumm0b0y!@cluster0.0btoh.azure.mongodb.net/https://twfm.herokuapp.com/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
