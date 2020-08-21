8a4914fb70983b4e04f830998be06bfeafe4adb0
Joe Cotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-08-21 19:31:35 -0400

    "updates"

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://joecotterchio:GeneralAssembly1@cluster0.0btoh.azure.mongodb.net/sei-joe",
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
+const MongoClient = require("mongodb").MongoClient;
+const uri ="mongodb+srv://joecotterchio:GeneralAssembly1@cluster0.0btoh.azure.mongodb.net/sei-joe?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 const db = mongoose.connection;
 db.on("connected", function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
