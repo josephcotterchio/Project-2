13f30fdb76c938bc9acda159254d2d0079013d6e
Joe Cotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-08-21 18:30:28 -0400

    "changed"

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/twfm", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb+srv://joecotterchio@gmail.com:GeneralAssembly1!@cluster0.0btoh.azure.mongodb.net/TWRM?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
 const db = mongoose.connection;
 db.on("connected", function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
