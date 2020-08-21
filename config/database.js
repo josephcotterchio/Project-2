da7f37518bcd3bce7d845b35cc137cbd3cceab18
joecotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-08-21 19:46:25 -0400

    "heroku"

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 const MongoClient = require("mongodb").MongoClient;
-const uri ="mongodb+srv://joecotterchio:GeneralAssembly1@cluster0.0btoh.azure.mongodb.net/sei-joe?retryWrites=true&w=majority";
+const uri ="mongodb+srv://joecotterchio:GeneralAssembly1@cluster0.0btoh.azure.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect((err) => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
+
 
 const db = mongoose.connection;
 db.on("connected", function () {
